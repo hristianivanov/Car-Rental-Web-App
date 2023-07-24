d8cbcaf329563995527ddd527a62e5fd25fe8aa0
Hristian Ivanow <92814022+hristianivanov@users.noreply.github.com>
2023-07-24 23:42:00 +0300

    Revert "remove unnecessary code in service extension"
    This reverts commit 0978254277a11a6bd3d3cb1e02831268f55faa41.

--- CSharpWebAdvanced-CourseProject/CarRentalSystem.Web.Infrastructure/Extensions/WebApplicationBuilderExtensions.cs
+++ CSharpWebAdvanced-CourseProject/CarRentalSystem.Web.Infrastructure/Extensions/WebApplicationBuilderExtensions.cs
@@ -1,47 +1,50 @@
 ﻿namespace CarRentalSystem.Web.Infrastructure.Extensions
 {
-	using System.Reflection;
+    using System.Reflection;
 
-	using Microsoft.Extensions.DependencyInjection;
+    using Microsoft.Extensions.DependencyInjection;
 
-	using CarRentalSystem.Services.Data;
-	using CarRentalSystem.Services.Data.Interfaces;
+    public static class WebApplicationBuilderExtensions
+    {
+        /// <summary>
+        /// This method registers all services with their interfaces and implementations of given assembly.
+        /// The assembly is taken from the type of any service implementation provided.
+        /// </summary>
+        /// <param name="serviceType"></param>
+        /// <exception cref="InvalidOperationException"></exception>
+        public static void AddApplicationServices(this IServiceCollection services, Type serviceType)
+        {
+            Assembly? serviceAssembly = Assembly.GetAssembly(serviceType);
+            if (serviceAssembly == null)
+            {
+                throw new InvalidOperationException("Invalid service type provided!");
+            }
 
-	public static class WebApplicationBuilderExtensions
-	{
-		/// <summary>
-		/// This method registers all services with their interfaces and implementations of given assembly.
-		/// The assembly is taken from the type of any service implementation provided.
-		/// </summary>
-		/// <param name="serviceType"></param>
-		/// <exception cref="InvalidOperationException"></exception>
-		public static void AddApplicationServices(this IServiceCollection services, Type serviceType)
-		{
-			Assembly? serviceAssembly = Assembly.GetAssembly(serviceType);
-			if (serviceAssembly == null)
-			{
-				throw new InvalidOperationException("Invalid service type provided!");
-			}
+            Type[] serviceTypes = serviceAssembly
+                .GetTypes()
+                .Where(t => t.Name.EndsWith("Service") && !t.IsInterface)
+                .ToArray();
 
-			Type[] serviceTypes = serviceAssembly
-				.GetTypes()
-				.Where(t => t.Name.EndsWith("Service") && !t.IsInterface)
-				.ToArray();
+            foreach (Type implementationType in serviceTypes)
+            {
+                Type? interfaceType = implementationType.GetInterface($"I{implementationType.Name}");
 
-			foreach (Type implementationType in serviceTypes)
-			{
-				Type? interfaceType = implementationType.GetInterface($"I{implementationType.Name}");
+                if (interfaceType == null)
+                {
+                    throw new InvalidOperationException($"No interface is provided for the service with name: {implementationType.Name}");
+                }
 
-				if (interfaceType == null)
-				{
-					throw new InvalidOperationException($"No interface is provided for the service with name: {implementationType.Name}");
-				}
+                //TODO: see if that logic works and whether it need it ?
+                if (implementationType.Name.StartsWith("IImage"))
+                {
+                    services.AddTransient(serviceType, implementationType);
+                }
 
-				services.AddScoped(serviceType, implementationType);
-			}
+                services.AddScoped(serviceType,implementationType);
+            }
+            //TODO: add start service
+            //services.AddScoped<ICarService, CarService>();
 
-			//TODO: see if that logic works and whether it need it ?
-			services.AddScoped<ICarService, CarService>();
-		}
-	}
+        }
+    }
 }
